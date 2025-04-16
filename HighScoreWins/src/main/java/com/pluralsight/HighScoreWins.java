b0212994aec83d6e7c5296c6c1a5fc840670d84a
Viktoriia Breha <viktoriia.breha@gmail.com>
2025-04-16 11:32:31 -0400

    HighScoreWins

--- HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
+++ HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
@@ -18,11 +18,16 @@ public class HighScoreWins {
         int score1 = Integer.parseInt(scores[0]);
         int score2 = Integer.parseInt(scores[1]);
 
+
         if (score1>score2) {
-            System.out.println("Winner: " + team1);
-        } else if (score1<score2) {
-            System.out.println("winner: " + team2);
-        } else {
+            System.out.printf("Winner: %s\n", team1);
+            //System.out.println("Winner: " + team1);
+        }
+        else if (score1<score2) {
+            //System.out.println("winner: " + team2);
+            System.out.printf("Winner: %s", team2);
+        }
+         else {
             System.out.println("Teams are tie");
         }
 
